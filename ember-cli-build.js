c63c032fa24de2a1147a46e00716ca8bc03e650b
@khaled.khalifa <khaled.khalifa@qonto.com>
2025-03-19 11:18:55 +0100

    prepend repo url

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,10 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  const app = new EmberApp(defaults, {
-    // Add options here
+  let app = new EmberApp(defaults, {
+    fingerprint: {
+      prepend: 'https://khaled-klod.github.io/Nasa/', // Adjust to match your GitHub Pages URL
+    },
   });
 
   return app.toTree();
